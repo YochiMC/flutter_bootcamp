5164cbcb3bd4b094b01b34a6fa942ac2a296a906
Joseph Martinez <josephmartinezcortes5@gmail.com>
2025-11-06 23:56:15 -0600

    update

--- segunda_tarea/lib/formulario.dart
+++ segunda_tarea/lib/formulario.dart
@@ -3,50 +3,138 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FormularioCuenta extends StatefulWidget {
   const FormularioCuenta({Key? key}) : super(key: key);
-
   @override
   State<FormularioCuenta> createState() => _FormularioCuentaState();
 }
 
 class _FormularioCuentaState extends State<FormularioCuenta> {
-  final _formkey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
 
   final TextEditingController _nombreController = TextEditingController();
-  final TextEditingController _apellidoPaternoController =
+  final TextEditingController _edadController = TextEditingController();
+  final TextEditingController _telefonoController = TextEditingController();
+  final TextEditingController _correoElectronicoController =
       TextEditingController();
-  final TextEditingController _apellidoMaternoController =
-      TextEditingController();
-  final TextEditingController _correoElectronico = TextEditingController();
-  final TextEditingController _contrasenia = TextEditingController();
+  final TextEditingController _contraseniaController = TextEditingController();
+
+  var _isOscured = true;
 
   void _enviarFormulario() async {
+    if (_formKey.currentState!.validate()) {
+      await FirebaseFirestore.instance.collection('Cuentas').add({
+        'nombre_usuario': _nombreController.text,
+        'edad_usuario': int.tryParse(_edadController.text),
+        'telefono_usuario': _telefonoController.text,
+        'correo_usuario': _correoElectronicoController.text,
+        'contrasenia': _contraseniaController.text,
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Formulario enviado exitosamente')),
+      );
 
+      _formKey.currentState!.reset();
+      _nombreController.clear();
+      _edadController.clear();
+      _telefonoController.clear();
+      _correoElectronicoController.clear();
+      _contraseniaController.clear();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return 
-Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.greenAccent,
-            title: Text('Crear Cuenta'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.greenAccent,
+        title: Text('Crear Cuenta'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                controller: _nombreController,
+                decoration: const InputDecoration(
+                  labelText: 'Nombre: ',
+                  hintText: 'nombre (s)',
+                ),
+                validator:
+                    (value) => value!.isEmpty ? 'Ingrese su nombre' : null,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                controller: _edadController,
+                decoration: const InputDecoration(
+                  labelText: 'Edad: ',
+                  hintText: 'de 18 a 60',
+                ),
+                validator: (value) {
+                  if(value == null || value.isEmpty){
+                    return 'Ingrese su edad';
+                  }
+                  final int? edad = int.tryParse(value);
+                  if(edad == null){
+                    return 'La edad debe de ser un número';
+                  }
+                  if(edad < 18 || edad >60){
+                    return 'La edad debe de estar entre 18 y 60';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _telefonoController,
+                decoration: const InputDecoration(
+                  labelText: 'Telefono Celular: ',
+                  hintText: 'xxxxxxxxxxx',
+                ),
+                validator:
+                    (value) =>
+                        value!.isEmpty ? 'Ingrese su número de telefono' : null,
+              ),
+              TextFormField(
+                controller: _correoElectronicoController,
+                decoration: const InputDecoration(
+                  labelText: 'Correo electrónico: ',
+                  hintText: 'example@domain.com',
+                ),
+                validator:
+                    (value) =>
+                        value!.isEmpty ? 'Ingrese su correo electrónico' : null,
+              ),
+              TextFormField(
+                obscureText: _isOscured,
+                controller: _contraseniaController,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    padding: const EdgeInsetsDirectional.only(end: 12.0),
+                    icon:
+                        _isOscured
+                            ? const Icon(Icons.visibility)
+                            : const Icon(Icons.visibility_off),
+                    onPressed: () {
+                      setState(() {
+                        _isOscured = !_isOscured;
+                      });
+                    },
+                  ),
+                  labelText: 'Contraseña: ',
+                  hintText: 'password',
+                ),
+                validator:
+                    (value) => value!.isEmpty ? 'Ingrese su contraseña' : null,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _enviarFormulario,
+                child: Text('Enviar'),
+              ),
+            ],
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Form(
-              key: _formkey,
-              child: ListView(
-                children: [
-                  TextFormField(
-                    controller: _nombreController,
-                    decoration: const InputDecoration(labelText: 'Nombre_: '),
-                    validator: (value) =>
-                    value!.isEmpty ? 'Ingrese su nombre' : null,
-                  )
-                ],
-              ),
-              ),
-            ),
-        );
+        ),
+      ),
+    );
   }
 }
--- segunda_tarea/lib/main.dart
+++ segunda_tarea/lib/main.dart
@@ -1,17 +1,19 @@
-  import 'package:flutter/material.dart';
-  import 'formulario.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'formulario.dart';
 
-  void main() {
-    runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const FormularioCuenta());
   }
-
-  class MainApp extends StatelessWidget {
-    const MainApp({super.key});
-
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        home: const FormularioCuenta(),
-      );
-    }
-  }
+}
