57ee6e20a88381416c872bf6a7380c7b45976c70
Joseph Martinez <josephmartinezcortes5@gmail.com>
2025-11-15 02:55:47 -0600

    update-colors-admin

--- proyecto_final/lib/assets/theme.dart
+++ proyecto_final/lib/assets/theme.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 //Colores admin
-const primaryColorAdmin = Color(0xDAFDBA);
-const secondaryColorAdmin = Color(0x9AEBA3);
-const alternativeColorAdmin = Color(0x45C4B0);
-const firstDarkColorAdmin = Color(0x13678A);
-const secondDarkColorAdmin = Color(0x012030);
+const primaryColorAdmin = Color.fromRGBO(218, 253, 186, 1);
+const secondaryColorAdmin = Color.fromRGBO(154, 235, 163, 1);
+const alternativeColorAdmin = Color.fromRGBO(69, 196, 176, 1);
+const firstDarkColorAdmin = Color.fromRGBO(19, 103, 138, 1);
+const secondDarkColorAdmin = Color.fromRGBO(1,32,48, 1);
 
 //Colores usuarios
 const primaryColorUser = Color(0xFFFFFF);
@@ -13,6 +13,10 @@ const secondaryColorUser = Color(0xDEEFE7);
 const alternativeColorUser = Color(0xB4BEC9);
 const firstDarkColorUser = Color(0x002333);
 const secondDarkColorUser = Color(0x159A9C);
+
+//Colores para fuentes
+const primaryColorFont = Colors.black;
+const secondaryColorFont = Colors.white;
 
 //Colores dinamicos (es una prueba XD)
 Map<String, Color> getTheme(String userType) {
